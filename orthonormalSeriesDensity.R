eda0da86cb85e3ca4ca14fcc3b625aa17b8d83f5
Eric Janofsky <ebjanofsky@gmail.com>
2016-01-31 08:17:39 -0600

    initial commit with example

---
+++ orthonormalSeriesDensity.R
@@ -0,0 +1,30 @@
+library(magrittr)
+
+
+# Orthogonal series density estimator
+# of Efromovich, 1999
+# Currently supports cosine basis
+orthonormalSeriesDensity<- function(data,J){
+  if(data%>%min<= 0 | data%>%max >1){
+    stop("Data needs to be scaled within [0,1]")
+  }
+  
+  # Get coefficients
+  orthCoeffs<- (1:J)%>%sapply(.,function(j)cosineBasis(data,j))%>%
+    apply(.,2,mean)
+  
+  plotDensityFn<- function(points){
+    (1:J)%>%sapply(.,function(j) cosineBasis(points,j) * orthCoeffs[j] )%>%
+      apply(.,1,sum) + 1
+                     
+    }
+    curve(plotDensityFn,0,1,ylim=c(0,1.5))
+    
+    plotDensityFn(data)
+}
+
+# Basis elements for cosine 
+# basis
+cosineBasis<-function(x,j){
+  sqrt(2) * cos(pi*j*x)
+}
---
+++ orthonormalSeriesDensityExample.R
@@ -0,0 +1,4 @@
+source("./orthonormalSeriesDensity.R")
+
+J=5
+orthonormalSeriesDensity(runif(1000,0,1),J)
