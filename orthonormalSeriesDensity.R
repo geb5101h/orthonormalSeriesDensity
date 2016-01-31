147abdd7d59868a7f0426368d95952b5188ac21a
Eric Janofsky <ebjanofsky@gmail.com>
2016-01-31 08:45:22 -0600

    return predictions

--- orthonormalSeriesDensity.R
+++ orthonormalSeriesDensity.R
@@ -4,28 +4,32 @@ library(magrittr)
 # Orthogonal series density estimator
 # of Efromovich, 1999
 # Currently supports cosine basis
-orthonormalSeriesDensity<- function(data,J){
-  if(data%>%min<= 0 | data%>%max >1){
+orthonormalSeriesDensity <- function(data,J) {
+  if (data %>% min <= 0 | data %>% max > 1) {
     stop("Data needs to be scaled within [0,1]")
   }
   
   # Get coefficients
-  orthCoeffs<- (1:J)%>%sapply(.,function(j)cosineBasis(data,j))%>%
+  orthCoeffs <- (1:J) %>% sapply(.,function(j)
+    cosineBasis(data,j)) %>%
     apply(.,2,mean)
   
-  plotDensityFn<- function(points){
-    (1:J)%>%sapply(.,function(j) cosineBasis(points,j) * orthCoeffs[j] )%>%
+  plotDensityFn <- function(points) {
+    (1:J) %>% sapply(.,function(j)
+      cosineBasis(points,j) * orthCoeffs[j]) %>%
       apply(.,1,sum) + 1
   }
   
-  predictions<- plotDensityFn(data)
-
-  curve(plotDensityFn,0,1,ylim=c(0,max(predictions)*1.001))
-    
+  predictions <- plotDensityFn(data)
+  
+  curve(plotDensityFn,0,1,ylim = c(0,max(predictions) * 1.001))
+  
+  predictions
+  
 }
 
-# Basis elements for cosine 
+# Basis elements for cosine
 # basis
-cosineBasis<-function(x,j){
-  sqrt(2) * cos(pi*j*x)
+cosineBasis <- function(x,j) {
+  sqrt(2) * cos(pi * j * x)
 }
