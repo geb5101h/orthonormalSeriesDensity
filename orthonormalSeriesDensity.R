894e9781a197cdd8cc6d59e9c0f553c73ae644a7
Eric Janofsky <ebjanofsky@gmail.com>
2016-01-31 09:15:59 -0600

    added shrinkage term

--- orthonormalSeriesDensity.R
+++ orthonormalSeriesDensity.R
@@ -5,24 +5,29 @@ library(magrittr)
 # of Efromovich, 1999
 # Currently supports cosine basis
 orthonormalSeriesDensity <- function(data,J) {
-  if (data %>% min <= 0 | data %>% max > 1) {
+  if (data %>% min < 0 | data %>% max > 1) {
     stop("Data needs to be scaled within [0,1]")
   }
+  
+  n = data%>%length
   
   # Get coefficients
   orthCoeffs <- (1:J) %>% sapply(.,function(j)
     cosineBasis(data,j)) %>%
     apply(.,2,mean)
   
+  # Get smoothing coefficients
+  smooth<- (1-1/(n*orthCoeffs^2))%>%sapply(.,function(x)max(0,x))
+  
   plotDensityFn <- function(points) {
     (1:J) %>% sapply(.,function(j)
-      cosineBasis(points,j) * orthCoeffs[j]) %>%
+      cosineBasis(points,j) * orthCoeffs[j]*smooth[j]) %>%
       apply(.,1,sum) + 1
   }
   
   predictions <- plotDensityFn(data)
   
-  curve(plotDensityFn,0,1,ylim = c(0,max(predictions) * 1.001))
+  curve(plotDensityFn,0,1,,ylim = c(min(predictions),max(predictions) * 1.001))
   
   predictions
   
--- orthonormalSeriesDensityExample.R
+++ orthonormalSeriesDensityExample.R
@@ -1,4 +1,13 @@
+rm(list=ls())
 source("./orthonormalSeriesDensity.R")
 
 J=5
-orthonormalSeriesDensity(runif(1000,0,1),J)
+n=1000
+#simulate a mixture of two Gaussians
+y= rbinom(n,1,.3)
+x= rnorm(n,.1,.1)*y + rnorm(n,.7,.1)*(1-y)
+x[x>=1] = 1; x[x<=0]=0
+
+par(mfcol=c(2,1))
+orthonormalSeriesDensity(x,J)
+hist(x,breaks=50)
