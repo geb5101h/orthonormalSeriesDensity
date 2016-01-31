a3016d081400249f0bf7540b6eb8d0326dd7008c
Eric Janofsky <ebjanofsky@gmail.com>
2016-01-31 11:49:17 -0600

    added new example and spatial inhomogeneity term

--- orthonormalSeriesDensity.R
+++ orthonormalSeriesDensity.R
@@ -4,27 +4,43 @@ library(magrittr)
 # Orthogonal series density estimator
 # of Efromovich, 1999
 # Currently supports cosine basis
-orthonormalSeriesDensity <- function(data) {
+orthonormalSeriesDensity <- function(data,highFreq = F) {
   if (data %>% min < 0 | data %>% max > 1) {
     stop("Data needs to be scaled within [0,1]")
   }
   
-  n = data%>%length
-  Jn = floor(4+0.5*log(n))
+  n = data %>% length
+  Jn = floor(4 + 0.5 * log(n))
+  Jmax = 6 * Jn
   # Get coefficients
-  orthCoeffs <- (1:Jn) %>% sapply(.,function(j)
+  orthCoeffs <- (1:Jmax) %>% sapply(.,function(j)
     cosineBasis(data,j)) %>%
     apply(.,2,mean)
   
-  Jrange = (1:Jn)%>%sapply(.,function(j){2/n - orthCoeffs[j]^2})
-  Jn = Jrange%>%cumsum%>%which.min
-  print(paste("J chosen to be",Jn))
+  Jrange = (1:Jn) %>% sapply(.,function(j) {
+    2 / n - orthCoeffs[j] ^ 2
+  })
+  Jselect = Jrange %>% cumsum %>% which.min
+  
+  print(paste("Jn chosen to be",Jselect))
   # Get smoothing coefficients
-  smooth<- (1-1/(n*orthCoeffs^2))%>%sapply(.,function(x)max(0,x))
+  smooth <- (1 - 1 / (n * orthCoeffs ^ 2)) %>% sapply(.,function(x)
+    max(0,x))
   
   plotDensityFn <- function(points) {
-    (1:Jn) %>% sapply(.,function(j)
-      cosineBasis(points,j) * orthCoeffs[j]*smooth[j]) %>%
+    (1:Jmax) %>% sapply(.,function(j) {
+      if (j <= Jn) {
+        cosineBasis(points,j) * orthCoeffs[j] * smooth[j]
+      }
+      else
+        if ((orthCoeffs[j] ^ 2 > 4 * log(n) / n)) {
+          print(paste("Included basis element",j))
+          
+          cosineBasis(points,j) * orthCoeffs[j]
+        }else
+          points * 0
+      
+    }) %>%
       apply(.,1,sum) + 1
   }
   
--- orthonormalSeriesDensityExample.R
+++ orthonormalSeriesDensityExample.R
@@ -1,12 +1,12 @@
 rm(list=ls())
 source("./orthonormalSeriesDensity.R")
 
-set.seed(12324)
+#set.seed(12324)
 J=5
-n=1000
+n=100
 #simulate a mixture of two Gaussians
 y= rbinom(n,1,.3)
-x= rnorm(n,.1,.1)*y + rnorm(n,.7,.1)*(1-y)
+x= rnorm(n,.3,.1)*y + rnorm(n,.7,.1)*(1-y)
 x[x>=1] = 1; x[x<=0]=0
 
 par(mfcol=c(2,1))
